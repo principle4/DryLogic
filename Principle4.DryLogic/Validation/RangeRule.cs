8b84cabb757d427798040e8e67e209d7a6d931a2
Trevor Levitt <trevor.levitt@gmail.com>
2016-07-05 10:03:38 -0700

    T.Levitt Added missing validator files

---
+++ Principle4.DryLogic/Validation/RangeRule.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Principle4.DryLogic.Validation
+{
+  public class RangeRule : PropertyRule
+  {
+    public object MinimumValue { get; private set; }
+    public object MaximumValue { get; private set; }
+    private Func<object, object> Conversion
+    {
+      get;
+      set;
+    }
+
+    public RangeRule(PropertyDefinition propertyDefinition, object minValue, object maxValue) : base(propertyDefinition)
+    {
+      MinimumValue = minValue;
+      MaximumValue = maxValue;
+
+      base.Assertion = oi =>
+      {
+        var stringValue = oi.GetUntypedValue(propertyDefinition).StringValue;
+        if (stringValue == null)
+        {
+          return true;
+        }
+
+        Type propertyType = propertyDefinition.ValueType;
+        object min;
+        object max;
+
+        try
+        {
+          min = Convert.ChangeType(MinimumValue, propertyType);
+          max = Convert.ChangeType(MaximumValue, propertyType);
+        }
+        catch (InvalidCastException cx)
+        {
+          return false;
+        }
+
+        var objValue = oi.GetUntypedValue(propertyDefinition).Value;
+        if (propertyType == typeof(int))
+        {
+          return (int)objValue >= (int)minValue && (int)objValue <= (int)maxValue;
+        }
+        if (propertyType == typeof(double) || propertyType == typeof(float))
+        {
+          return (double)objValue >= (double)minValue && (double)objValue <= (double)maxValue;
+        }
+        if (propertyType == typeof(DateTime) || propertyType == typeof(DateTime))
+        {
+          return (DateTime)objValue >= (DateTime)minValue && (DateTime)objValue <= (DateTime)maxValue;
+        }
+        throw new InvalidOperationException("Range type must be one of the following types: int, float, double, DateTime");
+      };
+
+      base.ErrorMessageGenerator = () =>
+        $"'{propertyDefinition.CurrentName}' must be between {MinimumValue} and {MaximumValue}.";
+    }
+  }
+}
---
+++ Principle4.DryLogic/Validation/StringLengthRule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Principle4.DryLogic.Validation
+{
+  public class StringLengthRule : PropertyRule
+  {
+    public int MinimumLength { get; private set; }
+    public int MaximumLength { get; private set; }
+    public StringLengthRule(PropertyDefinition propertyDefinition, int minLength, int maxLength) : base(propertyDefinition)
+    {
+      MinimumLength = minLength;
+      MaximumLength = maxLength;
+
+      base.Assertion = oi =>
+      {
+        var stringValue = oi.GetUntypedValue(propertyDefinition).StringValue;
+        if (stringValue == null)
+        {
+          return true;
+        }
+        return stringValue.Length >= MinimumLength && stringValue.Length <= MaximumLength;
+      };
+
+      base.ErrorMessageGenerator = () => String.Format("'{0}' must be between {1} and {2}.", propertyDefinition.CurrentName, MinimumLength, MaximumLength);
+    }
+
+  }
+}
