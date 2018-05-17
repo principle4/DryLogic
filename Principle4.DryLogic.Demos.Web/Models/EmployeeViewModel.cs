71abd673e3119e5957b980e6a117e94a43c95952
b-levitt <brandon.levitt@gmail.com>
2018-05-17 14:01:26 -0400

    Changed web sample to use "alternate" way of providing options to a drop down list.

--- Principle4.DryLogic.Demos.Web/Models/EmployeeViewModel.cs
+++ Principle4.DryLogic.Demos.Web/Models/EmployeeViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using Principle4.DryLogic.Tests;
 
 namespace Principle4.DryLogic.Demos.Web.Models
@@ -9,6 +10,13 @@ namespace Principle4.DryLogic.Demos.Web.Models
   public class EmployeeViewModel
   {
     public Employee MyEmployee { get; set; }
+
+        public List<SelectListItem> ScoreOptions = new List<SelectListItem> {
+          new SelectListItem() { Value = "0" , Text = "Red"  },
+          new SelectListItem() { Value = "1" , Text = "Blue"  },
+          new SelectListItem() { Value = "2" , Text = "Green"  }
+    };
+
     public EmployeeViewModel()
     {
       MyEmployee = new Employee();
